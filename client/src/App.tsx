aabe163d3f43d637ce16f2e7ac0d7697ece3c630
adamrayw <raywibowo68@gmail.com>
2023-01-07 23:05:08 +0700

    delete default content, add nav and hero component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./components/Hero/Hero";
+import Navs from "./components/Nav/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navs />
+      <Hero />
     </div>
   );
 }
