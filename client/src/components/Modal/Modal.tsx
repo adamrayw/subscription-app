d7a22439c98c64dcbdb3b0e39697e3388ab2f76d
adamrayw <raywibowo68@gmail.com>
2023-01-12 21:43:33 +0700

    add interface, dynamic modal

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,33 +1,50 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
-export default function ModalComponent() {
-    const [show, setShow] = useState(false);
+interface ModalProps {
+  text: string
+  variant: "primary" | "secondary"
+}
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+export default function ModalComponent({text, variant}: ModalProps) {
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}  size="lg">
-        Launch demo modal
+      <Button variant={variant} onClick={handleShow} size="lg" style={{marginRight: "1rem", width: "100%"}}>
+        {text}
       </Button>
 
       <Modal show={show} onHide={handleClose} centered>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>{text}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Form>
+        <Modal.Body>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control name="email" type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control name="password" type="password" placeholder="Password" />
+            </Form.Group>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
+            <Button variant={variant} type="submit">
+              {text}
+            </Button>
         </Modal.Footer>
+          </Form>
       </Modal>
     </>
   );
