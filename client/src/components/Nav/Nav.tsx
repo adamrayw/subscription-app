7ea440358e9abab5af1e2b65e4bd13d9114a75f3
adamrayw <raywibowo68@gmail.com>
2023-01-25 22:38:28 +0700

    add router

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,18 +1,20 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 export default function Navs() {
   return (
     <Navbar>
-    <Container>
-      <Navbar.Brand href="#home">FeedUs</Navbar.Brand>
-      {/* <Nav className="me-auto">
+      <Container>
+        <Navbar.Brand>
+          <Link to="/">FeedUs</Link>
+        </Navbar.Brand>
+        {/* <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">Features</Nav.Link>
         <Nav.Link href="#pricing">Pricing</Nav.Link>
       </Nav> */}
-    </Container>
-  </Navbar>
-  )
+      </Container>
+    </Navbar>
+  );
 }
