e9b780e3f77fe17f476458fb3b15492d20d6cd95
adamrayw <raywibowo68@gmail.com>
2023-01-28 09:54:02 +0700

    use state to display logout

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,19 +1,34 @@
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../context";
+import { Nav } from "react-bootstrap";
 
 export default function Navs() {
+  const [state, setState] = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    setState({ data: null, loading: false, error: null });
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
   return (
     <Navbar>
       <Container>
         <Navbar.Brand>
           <Link to="/">FeedUs</Link>
         </Navbar.Brand>
-        {/* <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav> */}
+        {state.data && (
+          <Nav.Link>
+            <NavLink onClick={handleLogout} to={""}>
+              Logout
+            </NavLink>
+          </Nav.Link>
+        )}
       </Container>
     </Navbar>
   );
