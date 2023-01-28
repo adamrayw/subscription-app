ce8ded816db73fe8bb7da0f0fbf5bd93cb04f612
adamrayw <raywibowo68@gmail.com>
2023-01-28 09:52:06 +0700

    protect articles route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import Hero from "./components/Hero/Hero";
 import Navs from "./components/Nav/Nav";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Article from "./pages/Article";
+import { ProtectedRoute } from "./routes/ProtectedRoutes";
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
       <Navs />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/articles" element={<ProtectedRoute />}>
+          <Route path="/articles" element={<Article />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
